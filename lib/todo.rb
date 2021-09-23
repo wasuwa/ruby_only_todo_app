7eb92c5a9c422e7f83fbb51713513b64732e2362
suwa <baru.web.production@gmail.com>
2021-09-23 12:25:19 +0900

    タスクを一覧表示できる機能を作成する

--- lib/todo.rb
+++ lib/todo.rb
@@ -5,6 +5,12 @@ class Todo
     @sql = SQL.new('../database.yml', 'tasks')
   end
 
+  def list
+    @sql.index.each do |record|
+      puts change_format_of_task(record)
+    end
+  end
+
   def create(title: 'タイトルなし', content: '内容なし', goal_at: default_goal_at)
     @sql.create(all_columns, [title, content, goal_at])
   end
@@ -26,8 +32,18 @@ class Todo
     def tomorrow
       24 * 60 * 60
     end
+
+    def change_format_of_task(task)
+      line = '-' * 40
+      <<~TEXT
+        #{line}
+        id:           #{task['id']}
+        タイトル:     #{task['title']}
+        内容:         #{task['content']}
+        完了予定時刻: #{task['goal_at']}
+        #{line}
+      TEXT
+    end
 end
 
-# SQL.new('../database.yml', 'tasks').index.each do |i|
-#   puts i
-# end
+Todo.new.list
